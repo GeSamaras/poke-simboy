b63a52b053c84e501deba30060bb62620f76416b
GeSamaras <georgiosamaras@hotmail.com>
2024-03-06 17:29:00 +0100

    collision boundaries and faces

--- index.js
+++ index.js
@@ -14,16 +14,44 @@ for (let i = 0; i < collisions.length; i+= 70) {
 
 
 class Boundary {
+    static width = 64
+    static height = 64
     constructor({position}) {
         this.position = position
         this.width = 64 // map resolution is 400%
         this.height = 64 // so this should be 400% of 16px
     }
     draw() {
-        drawContext.fillRect(this.position.x, this.position.y, this,width, this.height)
+        drawContext.fillStyle = 'red'
+        drawContext.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
 
+
+const boundaries = []
+
+// putting the camera offset here
+// so that the collisions map renders it in the proper position
+const offset = {
+    x: -100,
+    y: -500
+}
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 864)
+        boundaries.push(
+            new Boundary({
+                position: {
+                    x: j * Boundary.width + offset.x ,
+                    y: i * Boundary.height + offset.y
+                }
+            })
+        )
+    })
+})
+
+console.log(boundaries)
 
 // drawing images to the canvas, done through js' Image()
 // instead of calling the src like in html
@@ -35,20 +63,51 @@ playerImage.src = './tilesets/MaskFrog/SeparateAnim/Idle.png'
 
 
 class Sprite {
-    constructor({position, velocity, image}) {
+    constructor({position, velocity, image, frames = {max: 1} }) {
         this.position = position
         this.image = image
+        this.frames = frames
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max
+            this.height = this.image.height
+        }
     }
 
     draw() {
-        drawContext.drawImage(this.image, this.position.x, this.position.y)
+        drawContext.drawImage(
+            this.image,
+            // arguments for image croppping
+            0,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,       
+            this.image.width / this.frames.max,
+            this.image.height
+            )
+        }
     }
-}
+    
+    
+
+
+const player = new Sprite ({
+    position: {
+        x: canvas.width / 2 - 64 / 4 / 2,
+        y: canvas.height / 2 - 16 / 2
+    },
+    image: playerImage,
+    frames: {
+        max: 4
+    }
+})
+
 
 const background = new Sprite ({
     position: {
-        x: -100,
-        y: -500
+        x: offset.x,
+        y: offset.y
     },
     image: image
 })
@@ -72,33 +131,67 @@ const keys = {
     },
 }
 
+const testBoundary = new Boundary ({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
+
+
+// array to make items in the map moveable
+// according to the player's movement
+const movables = [background, testBoundary]
+
+// checks for player sprite to be hitting any of the four sides
+// of the collision box
+function rectangularCollision({rectangle1, rectangle2}) {
+    return (
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+        rectangle1.position.x + rectangle1.position.x >= rectangle2.width &&
+        rectangle1.position.y + rectangle1.position.y >= rectangle2.height &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+    )
+}
 
 function animate() {
     window.requestAnimationFrame(animate)
-    background.draw()
-    drawContext.drawImage(playerImage,
-        // arguments for image croppping
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,       
-        // image positioning and centering
-        canvas.width / 2 - playerImage.width / 4 / 2, 
-        canvas.height / 2 - playerImage.height / 2,
-        playerImage.width / 4,
-        playerImage.height
+    background.draw()/* 
+    boundaries.forEach(boundary => {
+        boundary.draw()
+    }) */
+    testBoundary.draw()
+    player.draw()
+
+
+    if (
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: testBoundary
+        })
     )
+
+
+
 
     // changing the background coordenates for movement
     // adding/subtracting 3px per frame
     if (keys.w.pressed && lastKey === 'w') {
-        background.position.y += 3
+        movables.forEach((movable) => {
+            movable.position.y += 3
+        })
     } else if (keys.a.pressed && lastKey === 'a') {
-        background.position.x += 3
+        movables.forEach((movable) => {
+            movable.position.x += 3
+        })
     }   else if (keys.s.pressed && lastKey === 's') {
-        background.position.y -= 3
+        movables.forEach((movable) => {
+            movable.position.y -= 3
+        })
     } else if (keys.d.pressed && lastKey === 'd') {
-        background.position.x -= 3
+        movables.forEach((movable) => {
+            movable.position.x -= 3
+        })
     }
 }
 
